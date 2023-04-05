bf330c2a7e2ce65da2e8c274a23258431759f374
kamille <kamille.senai@gmail.com>
2023-04-05 14:40:44 -0300

    pasta backend organizada

---
+++ Estrutura-de-repeticao/While/Program.cs
@@ -0,0 +1,60 @@
+﻿//------------nome---------------------//
+Console.WriteLine($"Digite seu nome:");
+string nome = Console.ReadLine();
+
+while (nome== "")
+{
+    Console.WriteLine($"campo *Nome* não pode estar Vazio");
+    System.Console.WriteLine("digite novamente:");
+    nome = Console.ReadLine();
+}
+
+//-------------idade-------------------//
+Console.WriteLine($"Digite sua idade:");
+int idade = int.Parse (Console.ReadLine());
+
+while (idade > 100 || idade <= 0 )
+{
+    Console.WriteLine($"Campo *Idade* incoreto: deve ser entre 0 e 100");
+    System.Console.WriteLine("digite novamente:");
+    idade = int.Parse (Console.ReadLine());
+}
+
+//-------------salario-------------------//
+Console.WriteLine($"Digite seu salário:");
+float salario = float.Parse (Console.ReadLine());
+
+while (salario == 0)
+{
+    Console.WriteLine($"salário não pode ser igual a 0");
+    Console.WriteLine($"Digite novamente:");
+    
+    salario = float.Parse (Console.ReadLine());
+    
+}
+
+//--------------estado civíl------------------------//
+Console.WriteLine(@$"Digite seu Estado Civil com:
+  s para (solteiro(a)),
+  c para (casado(a)),
+  v para(viuvo(a)),
+  d para(divorciado(a):");
+  char eCivil = char.Parse (Console.ReadLine());
+
+  while (eCivil != 's' && eCivil != 'c' && eCivil != 'v' && eCivil != 'd')
+{
+  Console.WriteLine(@$"Digite da forma que foi pedido :
+  s para (solteiro(a)),
+  c para (casado(a)),
+  v para(viuvo(a)),
+  d para(divorciado(a)");
+
+    System.Console.WriteLine("digite novamente:");
+    eCivil = char.Parse (Console.ReadLine());
+    
+}
+
+Console.WriteLine($"Informações registradas com Sucesso!!");
+
+
+
---
+++ atividade-extra/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine($"Digite o seu ano de nascimento:");
+  int nasc= int.Parse (Console.ReadLine());
+
+//DateTime.now : mostra data e hora atualizada.
+//DateOnly.fromdatetime : oculta a hora .
+var today = DateOnly.FromDateTime(DateTime.Now);
+
+// subtrai os anos do datetime (irá subtrair pelo ano de nascimento que o a pessoa colocar)
+var novaData = today.AddYears(-nasc);
+
+//novaData.Year : mostra apenas o ano.
+// Console.WriteLine(novaData.Year);
+
+//multiplica o ano por 52(tanto de semanas em 1 ano)
+int semanas = novaData.Year * 52;
+Console.WriteLine($"a sua idade atual é de {novaData.Year} anos e  {semanas} semanas");
