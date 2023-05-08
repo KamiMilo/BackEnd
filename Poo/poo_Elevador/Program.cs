95e8aed98134a845b0d76345240384f19012ec9c
kamille <kamille.senai@gmail.com>
2023-05-08 06:43:10 -0300

    .

--- Poo/poo_Elevador/Program.cs
+++ Poo/poo_Elevador/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using poo_elevador;
+
+Elevador Ev = new Elevador();
+
+ for (int i = 0; i < Ev.Capacidade; i++)
+       {
+            Console.WriteLine(Ev.Pessoas[i]);
+            i++;
+
+        }
+
+
+    Console.WriteLine($"Bem-vindo ao sistema do elevador");
+    Console.WriteLine($"deseja entrar?");
+    string opcao =Console.ReadLine();
+    
+    switch (opcao)
+    {
+        case "sim":
+        Ev.Entrar();
+        break;
+        default:
+            break;
+    }
+    
+    
+    
